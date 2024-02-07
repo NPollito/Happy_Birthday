8b360cd2ac7fe1519291125f92263d7f7f332f3f
NPollito <nilton_pv@hotmail.com>
2024-02-06 22:32:22 -0500

    happy brithday

---
+++ main.js
@@ -0,0 +1,90 @@
+import messages from "./messages.js"
+
+// crear fondo
+for (let i = 0; i <= 75; i++) {
+    let stars = document.createElement('DIV')
+    stars.classList.add('star')
+    stars.style.zIndex = "-1"
+
+    let size = Math.random() * 20
+    stars.style.fontSize = 10 + size+'px'
+
+    stars.style.left = Math.random () * + innerWidth + 'px'
+    stars.style.top = Math.random () * + innerHeight + 'px'
+    stars.style.filter = `hue-rotate(${i * 5}deg)`
+
+    document.querySelector('body').appendChild(stars)
+}
+
+function animatedStart() {
+            
+    let allStarts = document.querySelectorAll('.star')
+    let num = Math.floor(Math.random()*allStarts.length)
+
+    allStarts[num].classList.toggle('animate')
+}
+
+setInterval(animatedStart, 50)
+
+// crear cards
+const backPage = document.querySelectorAll('.back-page')
+
+backPage.forEach(card =>{
+    
+    return card.innerHTML = `
+        <article class="card"></article>
+        <article class="card"></article>
+        <article class="card"></article>
+    `
+})
+
+// agregar notas a los cards
+const card = document.querySelectorAll('.card')
+card.forEach(alert => {
+    return alert.addEventListener('click', note)
+})
+
+// function para crear notas
+function note() {
+    let randomMessage = Math.floor(Math.random() * messages.length)
+    
+    const alert = document.createElement('ASIDE')
+    alert.classList.add('note')
+    alert.innerHTML = `
+        <section class="container_note">
+            <span class="container_message">
+                <p>${messages[randomMessage]}</p>
+                <span class="close" title="cerrar">x</span>
+            </span>
+        </section>
+    `
+
+    // boton cerrar ventana
+    alert.querySelector('.close').addEventListener('click', () => {
+        alert.remove()
+    })
+
+    return document.body.appendChild(alert)
+}
+
+// effect card
+const flower = document.querySelector('.flower')
+
+flower.addEventListener('click', () => {
+   const container = document.querySelector('.container-card')
+   container.style.transform = 'scale(.6)'
+
+   setTimeout(() => {
+    document.querySelector('.left-layer').style.transform = 'rotateY(-180deg)'
+    document.querySelector('.right-layer').style.transform = 'rotateY(-180deg)'
+    
+    
+    setTimeout(() => {
+        document.querySelector('.top-layer').style.transform = 'rotateX(-180deg)'
+        document.querySelector('.botton-layer').style.transform = 'rotateX(-180deg)'
+
+    }, 600)
+
+   }, 600) 
+})
+
---
+++ messages.js
@@ -0,0 +1,28 @@
+const messages = [
+    'Feliz cumpleaños, que este nuevo año de vida te traiga mucha felicidad, amor y éxito.',
+    'Que cada día de este próximo año sea un motivo para sonreír. ¡Feliz cumpleaños!',
+    'Eres una persona maravillosa y hoy es un día igual de especial como lo eres tú',
+    'Ojalá que un millón de cosas buenas te caigan este año del cielo, feliz cumpleaños.',
+    'Que la vida te sonría siempre y que todos tus sueños se hagan realidad.',
+    'Cada año que pasa te hace más especial. ¡Feliz cumpleaños y que sigas brillando!',
+    '¡Feliz cumpleaños! Sonríe como tú sabes, disfruta de la alegría de estar con tu gente, y sigue siendo igual de maravilloso.',
+    '¡Que el brillo de tu personalidad ilumine cada rincón de tu vida en este nuevo año que comienzas hoy!',
+    'Que este año sumes experiencias inolvidables, risas interminables y éxitos inesperados.',
+    '¡Felicidades en tu día! Que este nuevo año de vida te traiga más alegría, más amor y más razones para sonreír.',
+    'En tu cumpleaños, celebremos no solo el paso del tiempo, sino las huellas imborrables que dejas en nuestros corazones.',
+    'Hoy no cuentan los años, ¡sino las sonrisas! Que este año te regale tantas como los momentos felices que nos has dado.',
+    '¡En tu día especial, que cada momento sea tan brillante como las velas en tu pastel de cumpleaños!',
+    'Que este nuevo año de vida esté lleno de colores brillantes en el lienzo de tus días. ¡Feliz cumpleaños, que pintes un año inolvidable!',
+    'A medida que sumas un nuevo año a tu colección de recuerdos, recuerda que eres como un buen vino: ¡mejoras con el tiempo! ¡Feliz cumpleaños!',
+    'Las estrellas hoy brillan con más intensidad, porque es el día en que una persona asombrosa llegó a este mundo. ¡Feliz cumpleaños, que tu luz nunca deje de brillar!',
+    'Ojalá que un millón de cosas buenas te caigan este año del cielo, feliz cumpleaños.',
+    'Que en este nuevo viaje alrededor del sol, cada destello de luz sea un deseo hecho realidad. ¡Feliz y radiante cumpleaños!',
+    'El reloj de la vida sigue avanzando, y en tu día especial, que cada tic-tac te recuerde lo importante que eres para todos nosotros. ¡Feliz cumpleaños!',
+    '¡Hoy celebramos un año más de tus vueltas alrededor del sol! Que esta nueva vuelta esté llena de aventuras, risas y momentos inolvidables.',
+    '¡Disfruta, amiga, que nunca volverás a ser tan joven!',
+    'Que pases un día horroroso, cargado de lloros y malos momentos. Que no... que es broma. Solo pretendía ser original y seguro que nadie te había deseado un feliz cumpleaños de esta manera. ¡A divertirse!',
+    'Que la alegría que has difundido en el pasado vuelva a ti en este día. Te deseo un cumpleaños muy feliz.',
+    'Para celebrar tu cumpleaños había pensado en un crucero por el Caribe. ¿Te importaría regarme las plantas hasta que regrese? ¡Feliz cumpleaños!',
+]
+
+export default messages;
